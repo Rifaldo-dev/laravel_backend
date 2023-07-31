62d9b3638207c85a17893388dc7718fb01076701
rifaldo-dev <muhammadrifaldosaputra@gmail.com>
2023-07-31 12:55:24 +0700

    first commit

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Keren</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        p {
+            color: #666;
+            line-height: 1.5;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #333;
+            border-radius: 5px;
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .button:hover {
+            background-color: #555;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Selamat datang di Halaman Keren!</h1>
+        <p>Ini adalah contoh halaman web sederhana namun menarik.</p>
+        <p>Anda dapat menambahkan lebih banyak elemen HTML seperti gambar, tombol, dan banyak lagi untuk mempercantik halaman ini.</p>
+        <a href="#" class="button">Tombol</a>
+    </div>
+</body>
+</html>
